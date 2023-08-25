f4672040203ccd17da23315579dab1ef529e8093
kleberaluizio <kleberaluizio@gmail.com>
2023-08-25 20:16:59 -0300

    Creating Java Record to receive data from Json

--- screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ screenmatch-consumindo-webservice/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,7 +1,12 @@
 package br.com.alura.screenmatch.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo> {
+
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
@@ -11,6 +16,13 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb tituloOmdb) {
+        this.nome = tituloOmdb.title();
+        this.anoDeLancamento = Integer.parseInt(tituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(tituloOmdb.runtime().substring(0,2));
+
     }
 
     public String getNome() {
@@ -66,5 +78,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return  "nome='" + nome + '\'' +
+                ", anoDeLancamento=" + anoDeLancamento +'\'' +
+                ", Duracao=" + duracaoEmMinutos;
     }
 }
